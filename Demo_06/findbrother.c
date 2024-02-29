a2ef3653476cc3efecf83b9c8b328991ebe990dd
Sudar1977 <Sudar1977@gmail.com>
2024-02-29 15:15:11 +0300

    Fix

--- Demo_06/findbrother.c
+++ Demo_06/findbrother.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #define datatype int
+#define DEBUG
+
 
 typedef struct tree
 {
@@ -9,11 +11,11 @@ typedef struct tree
 } tree;
 
 
-tree * findBrother(tree *, int );
+tree *findBrother(tree *root, int key);
 
 int main(int argc, char ** argv)
 {
-    tree t[10]={0};
+    tree t[10]= {0};
 
     t[0].left   = t+1;
     t[0].right  = t+2;
@@ -54,7 +56,7 @@ int main(int argc, char ** argv)
     t[8].key    = 6;
     t[8].parent = t+4;
 
-int origin;
+    int origin;
     scanf("%d", &origin);
     tree *tmp = findBrother(t,origin);
     printf("%d\n", tmp==NULL? 0 : tmp->key);
@@ -62,64 +64,29 @@ int origin;
     return 0;
 }
 
-
-tree * findBrother(tree *root, int key)
+tree *findBrother(tree *root, int key)
 {
-
-int was_in_bottom;
-
-    tree * it = root;
-    tree * left_edge = root;
-    tree * right_edge = root;
-
-    while(NULL != left_edge && NULL != left_edge->left)
+    static tree *it = NULL;
+#ifdef DEBUG
+    printf("%d\n",root->key);
+#endif
+    if(root->key==key && root->parent)
     {
-        left_edge = left_edge->left;
-    }
-
-    while(NULL != right_edge && NULL != right_edge->right)
-    {
-        right_edge = right_edge->right;
-    }
-    it = left_edge;
-    while(it != right_edge)
-    {
-        if(NULL == it || NULL == it->parent)
+#ifdef DEBUG
+        printf("Find it: %d \n",root->key);
+#endif
+        if ( root == root->parent->left)
         {
-            continue;
+            it = root->parent->right;
         }
         else
         {
-            if(it->key == key)
-            {
-                if (it == it->parent->left)
-                {
-                    return it->parent->right;
-                }
-                else
-                {
-                    return it->parent->left;
-                }
-            }
-        }
-
-#ifdef DEBUG
-    printf("it: %d\n", it->key);
-#endif
-        if(it == it->parent->left && it->parent->right != NULL) // от левого идём к правому если он есть
-        {
-            it = it->parent->right;
-            was_in_bottom = 0;
-        }
-        else if (NULL != it->left && !was_in_bottom) //если уже на правом, смотрим, можно ли спуститься вниз
-        {
-            it = it->left;
-        }
-        else if(NULL != it->parent) //иначе идём вверх
-        {
-            it=it->parent;
-            was_in_bottom = 1;
+            it = root->parent->left;
         }
     }
-    return NULL;
+    if(root->left && !it)//! NULL
+        preorder(root->left,key);
+    if(root->right && !it)//! NULL
+        preorder(root->right,key);
+    return it;
 }
